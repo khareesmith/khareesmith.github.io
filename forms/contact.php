02969272c85ae4edbf18d7745b7cc5b7b7274594
khareesmith <kharee.smith@yahoo.com>
2020-09-27 15:22:53 -0400

    fixed nav link scrolling, updated information, added working contact

--- forms/contact.php
+++ forms/contact.php
@@ -15,7 +15,7 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  // Uncomment below to use SMTP to send emails. Need to enter correct SMTP credentials
   /*
   $contact->smtp = array(
     'host' => 'example.com',
